efb685f8dc14791a13a096956fad6bf0e518aa9f
icepants <shawnluther99@gmail.com>
2019-10-02 22:20:11 -0400

    Possible fix

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+// Node Packages.
+const mongoose = require('mongoose');
+
+// Define This As A Schema. 
+const Schema = mongoose.Schema;
+
+// Define Our Article Schema.
+var articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    articleDescribe: {
+        type: String,
+        required: true
+    },
+    isSaved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// Define and Export This Document As Schema Name 'Article'.
+var Article = mongoose.model("Article", articleSchema);
+// Do Export.
+module.exports = Article;
